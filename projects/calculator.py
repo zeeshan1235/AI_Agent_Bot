cc06fd2446c48161ed00c67f4b25854c2fd7b668
Zeeshan Ali <your-email@example.com>
2026-05-20 22:33:21 +0100

    Auto-generated app

---
+++ projects/calculator.py
@@ -0,0 +1,9 @@
+
+def calculator():
+    print("Welcome to Simple Calculator")
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    print(f"Sum: {a + b}")
+
+if __name__ == "__main__":
+    calculator()
